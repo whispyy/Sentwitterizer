f2c6933271299445e313f8ee09fe4f4e7dddb3a7
whispyy <jfdurk@hotmail.fr>
2015-09-22 10:59:14 +0200

    récupérer un tweet ok

---
+++ src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+import twitter4j.*;
+import twitter4j.conf.*;
+
+public class Main {
+	
+	/**
+	 * @param args
+	 * @throws TwitterException 
+	 */
+	public static void main(String[] args) throws TwitterException {
+		Configuration conf = (new ConfigurationBuilder()).setDebugEnabled(true)
+				  .setOAuthConsumerKey("5WHrltMd3j4YTA1YTZoUET8DT")
+				  .setOAuthConsumerSecret("kjnJ03shic0YKmtMA5tkA09gaw8cQLbPaLzbSCEevUBl2HDtA5")
+				  .setOAuthAccessToken("68107269-fWxJ2V3HlQNmVRVaSZoEXzU2KiC8an6h4o1WxR0tN")
+				  .setOAuthAccessTokenSecret("RO2Rg58MtSxzRTPd4hsPaEWQdhPnOvWicvBqkzDIZqOOu")
+				  .setHttpProxyHost("cache.univ-lille1.fr")
+				  .setHttpProxyPort(3128).build();
+		TwitterFactory tf = new TwitterFactory(conf);
+		Twitter twitter = tf.getInstance();
+		
+		Query query 		= new Query("source: whispyy");
+		QueryResult result 	= twitter.search(query);
+		for (Status status : result.getTweets()) {
+	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+	    }
+	}
+
+}
